ed2ddc6ff6d3969535f9c2c938e029f05ee83aea
Akumar <128833380+Adityaakr@users.noreply.github.com>
2023-11-08 14:15:39 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,20 @@
-/**
- * @license MIT
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+import { fetchData, url } from "./api";
+import * as module from "./module.js";``
+
+//@param {nodelist} elements -> Elements node array
+//@param {string eventType -> event type like ex -> click, mouseover
+//""   {*} callback function
+   
+const addEventOnElements = function(elements, eventType, callback){
+        for(const element of elements) element.addEventListener(eventType,callback);
+}
+
+//
+const searchView = document.querySelector("[data-search-view]");
+const searchToggler = document.querySelector("[data-search-toggler]");
+
+searchToggler.addEventListener("click", function() {
+  searchView.classList.toggle("icon-btn");
+});
